ec3173714bbdf4015fde0608350a286f42371eda
Joseph D <751728@student.dusd.net>
2024-11-18 10:22:57 -0800

    Worked on projecct.
    Finished damage zones and halfway done with sprite animations

--- 2DBeginnerAdventureP2Diaz/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP2Diaz/Assets/Scripts/EnemyController.cs
@@ -12,11 +12,15 @@ public class nemyController : MonoBehaviour
 
     float timer;
     int direction = 1;
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,10 +35,14 @@ public class nemyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move X", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
 
