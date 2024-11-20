e3cc16eeeff419df98df89f20a223b441d57fad1
Joseph D <751728@student.dusd.net>
2024-11-20 09:59:31 -0800

    worked project
    Finished sprit animations and halfway through next one

--- 2DBeginnerAdventureP2Diaz/Assets/Scripts/DuckoController.cs
+++ 2DBeginnerAdventureP2Diaz/Assets/Scripts/DuckoController.cs
@@ -8,7 +8,10 @@ public class DuckoController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+
+    public GameObject projectilePrefab;
+
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -19,11 +22,16 @@ public class DuckoController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+       
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         
     }
 
@@ -33,6 +41,17 @@ public class DuckoController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         if(isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -40,6 +59,10 @@ public class DuckoController : MonoBehaviour
             {
                 isInvincible = false;
             }
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
         }
     
     }
@@ -55,6 +78,8 @@ public class DuckoController : MonoBehaviour
     {
         if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
@@ -64,5 +89,15 @@ public class DuckoController : MonoBehaviour
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
